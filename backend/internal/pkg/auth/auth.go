05ba554e71f105a0c4aaf6e01fb3787925aee36f
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-02-16 04:48:17 +0900

    auth usecase

--- backend/internal/pkg/auth/auth.go
+++ backend/internal/pkg/auth/auth.go
@@ -1,8 +1,42 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const JWT_TOKEN_EXPIRATION = 60 * 60 * 24
+
+type Claims struct {
+	Email string `json:"email"`
+	jwt.StandardClaims
+}
 
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(bytes), err
+}
+
+func VerifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func CreateToken(email string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &Claims{
+		Email: email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(os.Getenv("JWT_SECRET"))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+
 }
---
+++ backend/internal/usecase/auth_usecase.go
@@ -0,0 +1,93 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/YukiOnishi1129/react-output-crud-auth-api/backend/internal/infrastructure/persistence/dto"
+	"github.com/YukiOnishi1129/react-output-crud-auth-api/backend/internal/pkg/auth"
+	apperrors "github.com/YukiOnishi1129/react-output-crud-auth-api/backend/internal/pkg/errors"
+	"github.com/YukiOnishi1129/react-output-crud-auth-api/backend/internal/repository"
+	"github.com/YukiOnishi1129/react-output-crud-auth-api/backend/internal/usecase/input"
+	"github.com/YukiOnishi1129/react-output-crud-auth-api/backend/internal/usecase/output"
+)
+
+
+
+type AuthUseCase interface {
+	Login(ctx context.Context, input *input.LoginInput) (*output.AuthOutput, error)
+	Register(ctx context.Context, input *input.RegisterInput) (*output.AuthOutput, error)
+}
+
+type authUseCase struct {
+	userRepo repository.UserRepository
+}
+
+func NewAuthUseCase(userRepo repository.UserRepository) AuthUseCase {
+	return &authUseCase{userRepo: userRepo}
+}
+
+
+func (u *authUseCase) Login(ctx context.Context, input *input.LoginInput) (*output.AuthOutput, error) {
+	if err := input.Validate(); err != nil {
+		return nil, apperrors.NewValidationError("invalid input parameters", err)
+	}
+	// find user by email
+	user, err := u.userRepo.FindByEmail(ctx, &dto.FindUserByEmailInput{
+		Email: input.Email,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// verify password
+	if err :=  auth.VerifyPassword(user.Password, input.Password); err != nil {
+		return nil, apperrors.NewUnauthorizedError("email or password is incorrect", err)
+	}
+
+	// create jwt token
+	tokenString, err := auth.CreateToken(user.Email)
+	if err != nil {
+		return nil, apperrors.NewInternalError("failed to create token", err)
+	}
+
+	userOutput := output.ConvertUserOutput(user)
+	return &output.AuthOutput{
+		Token: tokenString,
+		User:  *userOutput,
+	}, nil
+}
+
+
+func (u *authUseCase) Register(ctx context.Context, input *input.RegisterInput) (*output.AuthOutput, error) {
+	if err := input.Validate(); err != nil {
+		return nil, apperrors.NewValidationError("invalid input parameters", err)
+	}
+
+	// hash password
+	hashedPassword, err := auth.HashPassword(input.Password)
+	if err != nil {
+		return nil, apperrors.NewInternalError("failed to hash password", err)
+	}
+
+	// create user
+	user, err := u.userRepo.Create(ctx, &dto.CreateUserInput{
+		Name:     input.Name,
+		Email:    input.Email,
+		Password: hashedPassword,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// create jwt token
+	tokenString, err := auth.CreateToken(user.Email)
+	if err != nil {
+		return nil, apperrors.NewInternalError("failed to create token", err)
+	}
+
+	userOutput := output.ConvertUserOutput(user)
+	return &output.AuthOutput{
+		Token: tokenString,
+		User:  *userOutput,
+	}, nil
+}
