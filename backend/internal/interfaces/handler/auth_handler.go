102692730ed30dc4439893f2b78b0120eab06059
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-02-17 00:49:18 +0900

    CheckAuthentication

--- backend/internal/interfaces/handler/auth_handler.go
+++ backend/internal/interfaces/handler/auth_handler.go
@@ -17,6 +17,7 @@ type AuthHandler interface {
 	RegisterAuthHandlers(r *mux.Router)
 	Login(w http.ResponseWriter, r *http.Request)
 	Signup(w http.ResponseWriter, r *http.Request)
+	CheckAuthentication(w http.ResponseWriter, r *http.Request)
 }
 
 type authHandler struct {
@@ -28,14 +29,15 @@ func NewAuthHandler(authUseCase usecase.AuthUseCase) AuthHandler {
 	return &authHandler{authUseCase: authUseCase}
 }
 
-type AuthHandle func(w http.ResponseWriter, r *http.Request)
-
 
 func (h *authHandler) RegisterAuthHandlers(r *mux.Router) {
 	authRouter := r.PathPrefix(constants.AuthPath).Subrouter()
+	isAuthCheckRouter := r.PathPrefix(constants.AuthPath).Subrouter()
+	isAuthCheckRouter.Use(h.authMiddleware)
 
 	authRouter.HandleFunc("/login", h.Login).Methods(http.MethodPost,http.MethodOptions)
 	authRouter.HandleFunc("/signup", h.Signup).Methods(http.MethodPost,http.MethodOptions)
+	isAuthCheckRouter.HandleFunc("/authentication", h.CheckAuthentication).Methods(http.MethodPost,http.MethodOptions)
 }
 
 func (h *authHandler) Login(w http.ResponseWriter, r *http.Request) {
@@ -66,11 +68,24 @@ func (h *authHandler) Signup(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	output, err := h.authUseCase.RegisterUserInput(ctx, input)
+	output, err := h.authUseCase.RegisterUser(ctx, input)
 	if err != nil {
 		h.respondError(w, err)
 		return
 	}
 
 	h.respondJSON(w, http.StatusCreated, output)
+}
+
+func (h *authHandler) CheckAuthentication(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	email := h.getUserEmail(r)
+
+	output, err := h.authUseCase.CheckAuthentication(ctx, &input.CheckAuthenticationInput{Email: email})
+	if err != nil {
+		h.respondError(w, err)
+		return
+	}
+
+	h.respondJSON(w, http.StatusOK, output)
 }
